890011fd1bdca7f38c4bab9b71d9460fae5ef80e
ziyaltdinov911 <nsavekb@gmail.com>
2025-05-18 12:24:23 +0300

    Добавлена коллекция и настройка layout

--- iPocket/Module/MainScreenModule/Model/PostItem.swift
+++ iPocket/Module/MainScreenModule/Model/PostItem.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import CoreLocation
 
 class PostDate: Identifiable {
     let id = UUID().uuidString
@@ -89,29 +90,34 @@ class PostItem: Identifiable {
     let description: String?
     let isFavorite: Bool
     let date: Date
+    let location: CLLocationCoordinate2D?
     
-    init(photos: [String], comments: [Comment]?, tags: [String]?, description: String?, isFavorite: Bool = false, date: Date) {
+    init(photos: [String], comments: [Comment]?, tags: [String]?, description: String?, isFavorite: Bool = false, date: Date, location: CLLocationCoordinate2D? = nil) {
         self.photos = photos
         self.comments = comments
         self.tags = tags
         self.description = description
         self.date = date
         self.isFavorite = isFavorite
+        self.location = location
     }
     
     static func getMockItem() -> [PostItem] {
         return [
             PostItem(
                 photos: ["img1", "img2"],
-                comments: nil,
+                comments: [Comment(date: Date(), comment: "Lorem ipsum dolor sit amet, consectetur adipisicing elitLorem ipsum dolor sit amet, consectetur adipisicing elit"),
+                           Comment(date: Date(), comment: "Lorem ipsum dolor sit amet, consectetur adipisicing elitLorem ipsum dolor sit amet, consectetur adipisicing elit"),
+                           Comment(date: Date(), comment: "Lorem ipsum dolor sit amet, consectetur adipisicing elitLorem ipsum dolor sit amet, consectetur adipisicing elit")],
                 tags: ["Дом", "Nature"],
                 description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit",
                 isFavorite: true,
-                date: Date()
+                date: Date(),
+                location: CLLocationCoordinate2D(latitude: 40.728, longitude: -74)
             ),
             PostItem(
                 photos: ["img2", "img3"],
-                comments: nil,
+                comments: [Comment(date: Date(), comment: "2222Lorem ipsum dolor sit amet, consectetur adipisicing elit")],
                 tags: ["Дом", "Nature"],
                 description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit",
                 isFavorite: true,
